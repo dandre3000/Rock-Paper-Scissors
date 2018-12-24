18e6e1b02a7e79acf2aa1e1850d03cdc13dc7bb9
dandre3000 <deaundre.payne@gmail.com>
2018-12-24 07:32:25 -0500

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,40 +1,41 @@
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
 
-const RpsGame = require("./rps-game");
+const RpsGame = require('./rps-game');
 
 const app = express();
 
-const clientPath = "../client";
-console.log("Serving static from " + clientPath);
+const clientPath = `${__dirname}/../client`;
+console.log(`Serving static from ${clientPath}`);
 
 app.use(express.static(clientPath));
 
 const server = http.createServer(app);
 
-const io = socketio(server, {cookie: false});
+const io = socketio(server);
 
 let waitingPlayer = null;
 
-io.on("connection", (sock) => {
-	if (waitingPlayer) {
-		new RpsGame(waitingPlayer, sock);
-		waitingPlayer = null;
-	} else {
-		waitingPlayer = sock;
-		waitingPlayer.emit("message", "Waiting for an opponent...");
-	}
-	
-	sock.on("message", (text) => {
-		io.emit("message", text);
-	});
+io.on('connection', (sock) => {
+
+  if (waitingPlayer) {
+    new RpsGame(waitingPlayer, sock);
+    waitingPlayer = null;
+  } else {
+    waitingPlayer = sock;
+    waitingPlayer.emit('message', 'Waiting for an opponent');
+  }
+
+  sock.on('message', (text) => {
+    io.emit('message', text);
+  });
 });
 
-server.on("error", (err) => {
-	console.error("Server error:", err);
+server.on('error', (err) => {
+  console.error('Server error:', err);
 });
 
-server.listen(5000, () => {
-	console.log("RPS started on 5000");
+server.listen(8080, () => {
+  console.log('RPS started on 8080');
 });
